85644e46e754963a2ecb452e3cbcba6cd39d61a2
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2022-06-11 21:11:50 +0545

    Feature: complete coming soon page

---
+++ js/script.js
@@ -0,0 +1,29 @@
+function countDownDate(date) {
+    var now = new Date().getTime();
+    var distance = date - now;
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    document.getElementById("days").innerHTML = days;
+    document.getElementById("hours").innerHTML = hours;
+    document.getElementById("minutes").innerHTML = minutes;
+    document.getElementById("seconds").innerHTML = seconds;
+
+    if (distance < 0) {
+        clearInterval(x);
+        document.getElementById("days").innerHTML = "00";
+        document.getElementById("hours").innerHTML = "00";
+        document.getElementById("minutes").innerHTML = "00";
+        document.getElementById("seconds").innerHTML = "00";
+    }
+}
+
+function startCountDown() {
+    var countDownDate = new Date("August 1, 2022 15:37:25").getTime();
+    var x = setInterval(function () {
+        countDownDate(countDownDate);
+    }, 1000);
+}
